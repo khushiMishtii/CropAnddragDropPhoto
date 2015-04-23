8555bb0728dcfe4858a4d867d767bad5b836f1b8
khushiMishtii <khushi.cyber1@gmail.com>
2015-04-23 12:58:26 +0530

    Initial Commit

---
+++ CropAnddragDropPhoto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CropAnddragDropPhoto
+//
+//  Created by Khushboo Sharma on 4/23/15.
+//  Copyright (c) 2015 meMy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CropAnddragDropPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CropAnddragDropPhoto
+//
+//  Created by Khushboo Sharma on 4/23/15.
+//  Copyright (c) 2015 meMy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
